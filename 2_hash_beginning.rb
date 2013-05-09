d5a36f888af7fd76dae661c6963fdb4a86cc4f92
blackjaic <jamii@Catmacdu.local>
2013-05-09 11:18:16 -0600

    Added a second hash table, merging two tables, and checking key existance

--- 2_hash_beginning.rb
+++ 2_hash_beginning.rb
@@ -14,7 +14,7 @@ h.keys
 # Print all the values of the hash table.
 h.values
 
-# Iternation over the as table
+# Iteration over the as table
 #   This pulls out keys and values out
 #   one object at a time.
 #  Return value of the each is the hashtable.
@@ -22,15 +22,29 @@ h.each do |k|
 puts k
 end
 
-# Itermation over the keys
+# Iteration over the keys
 h.each_key do |k|
 puts k
 end
 
-# Iternation over the pairs (key => value)
+# Iteration over the pairs (key => value)
+puts "Iteration over the pairs (key => value)"
 h.each_pair do |k, v|
-puts k
-puts v
+puts "k=", k
+puts "v=", v
 end
+
+# Create a second hash table and merge it with the first.
+puts "Create a second hash table"
+snds = { "cat" => "meow", "bird" => "chirp" }
+m = h.merge(snds)
+# Note: the returned value is the merged array, h and snds are not changed.
+# To merge into a hash table use merge! This will make h be the merged
+# hash table. Also note that h and snds both have a "cat" key. The merged
+# table contains only the value from the second hash table.
+
+# How to check if a key exists in the hash table
+snds.has_key?("dog")
+snds.has_key?("cat")
 
 
